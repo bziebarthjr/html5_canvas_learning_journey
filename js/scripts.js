d59d6d2eff18a50f5769ac388c2119e4df9ffec9
Bruce Ziebarth <bziebarthjr@gmail.com>
2025-09-19 11:25:46 -0500

    Uploading initial files to git for HTML5 Canvas learning project

---
+++ js/scripts.js
@@ -0,0 +1,68 @@
+// Definitions
+const canvas = document.getElementById('hello-world-canvas');
+const context = canvas.getContext('2d');
+
+// set initial canvas size
+resizeCanvas();
+
+// Function to resize the canvas to fit the window
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+        
+    // Redraw content or handle other operations after resizing
+    draw();
+}
+
+// Function to draw on the canvas
+function draw() {
+    // Blue rectangle
+    context.fillStyle = "blue";
+    context.fillRect(10, 40, 30, 70);
+
+    // Yellow Rectangle
+    context.fillStyle = "yellow";
+    context.fillRect(50, 30, 60, 30);
+
+    // 
+}
+
+// Call resizeCanvas() when the window is resized
+window.addEventListener('resize', resizeCanvas);
+
+// inital draw
+draw();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
