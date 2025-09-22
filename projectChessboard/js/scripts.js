52b644804d1c73753f86a82de6316d02f138518d
Bruce Ziebarth <bziebarthjr@gmail.com>
2025-09-22 18:56:02 -0500

    Completed chessboard project and updated readme file

--- projectChessboard/js/scripts.js
+++ projectChessboard/js/scripts.js
@@ -16,30 +16,28 @@ function resizeCanvas() {
 
 // Function to draw on the canvas
 function draw() {
-  
-    // context.rect(xCoordinate, yCoordinate, width, height)
-    context.strokeStyle = 'green';
-    context.lineWidth = 11;
-    context.lineJoin = 'round';
-    context.fillStyle = '#99ccff';
-    context.rect(50, 50, 400, 200);
-    context.stroke();
-    context.fill();
+    // define variables
+    const lightCellColor = "#ddb180";
+    const darkCellColor = "#7c330C";
+    const size = 100;
+    const rows = 8
+    const cols = 8;
+    const ox = 100;
+    const oy = 100;
 
-    // context.fillRect(xCoordinate, yCoordinate, width, height)
-    context.fillStyle = "green";
-    context.fillRect(600, 50, 400, 200);
+    // Draw Frame
+    context.strokeStyle = 'black';
+    context.strokeRect(ox, oy, cols * size, rows * size);
 
-    // context.strokeRect(xCoordinate, yCoordinate, width, height)
-    context.lineWidth = 3;
-    context.strokeStyle = 'blue';
-    context.lineJoin = 'square';
-    context.strokeRect(50, 350, 400, 200);
-    
-    //context.clearRect(xCoordinate, yCoordinate, width, height)
-    // this will clean the whole canvas when uncommented
-    // context.clearRect(0, 0, canvas.width, canvas.height);
-}
+    // Draw and Color Cells
+    for (let r = 0; r < rows; r++) {
+        for (let c=0; c < cols; c++) {
+            const isDark = (r + c) % 2 === 0;
+            context.fillStyle = isDark ? darkCellColor : lightCellColor;
+            context.fillRect(ox + c * size, oy + r * size, size, size);
+        }
+    }
+} 
 
 // Call resizeCanvas() when the window is resized
 window.addEventListener('resize', resizeCanvas);
