ee07b96c87cd95098aef711bf984a3fb6fbf3f50
Bruce Ziebarth <bziebarthjr@gmail.com>
2025-09-23 08:49:06 -0500

    Completed lesson 8 practice polygons and shadow

--- lesson8_circlesPolygons/js/scripts.js
+++ lesson8_circlesPolygons/js/scripts.js
@@ -18,18 +18,82 @@ function resizeCanvas() {
 function draw() {
     // define radian variable
     const radian = Math.PI / 180;
+
+    // define polygon variables
+    const sides = 5;
+    const radius = 100;
+    let centerX = 300;
+    let centerY = 300;
+    const startAngle = 200;
+    angle = (2 * Math.PI) / sides;
   
     // context.arc(centerX, centerY, radius, startAngle, endAngle, antiClockwiseDirection);
     context.beginPath();
     context.strokeStyle = 'red';
     context.fillStyle = 'red';
     context.lineWidth = 15;
-    context.arc(800, 1000, 150, 135 * radian, 360 * radian, false);
+    context.arc(800, 300, 150, 135 * radian, 360 * radian, false);
     context.stroke();
     context.fill();
 
-    // draw pac-man
+    // draw polygon
+    context.beginPath();
+    context.strokeStyle = 'red';
+    context.lineWidth = 5;
+    context.lineJoin = 'round';
 
+    // Beginning Point Coordinates
+    const beginX = centerX + radius * Math.cos(startAngle);
+    const beginY = centerY - radius * Math.sin(startAngle);
+
+    // Draw Lines
+    for(var i = 1; i <= sides; i++) {
+        // Current point's coordinates
+        const currentAngle = startAngle + i * angle;
+        const currentPointX = centerX + radius * Math.cos(currentAngle);
+        const currentPointY = centerY - radius * Math.sin(currentAngle);
+
+        // Draw the line
+        context.lineTo(currentPointX, currentPointY);
+    }
+    
+    context.closePath();
+    context.shadowColor = 'black';
+    context.shadowOffsetX = 5;
+    context.shadowOffsetY = 5;
+    context.shadowBlur = 10;
+    context.stroke();
+
+    /* Shadow attributes on canvas
+    - shadowColor
+    - shadowOffsetX
+    - shadowOffsetY
+    - shadowBlur
+    */
+
+    // Rectangle
+    context.beginPath();
+    context.strokeStyle = 'green';
+    context.fillStyle = 'green';
+    context.shadowColor = 'black';
+    context.shadowOffsetX = 5;
+    context.shadowOffsetY = 5;
+    context.shadowBlur = 5;
+    context.rect(250, 700, 150, 400);
+    context.stroke();
+    context.fill();
+
+    // Circle
+    context.beginPath();
+    context.strokeStyle = 'red';
+    context.fillStyle = 'red';
+    context.shadowColor = 'black';
+    context.shadowOffsetX = -10;
+    context.shadowOffsetY = 15;
+    context.shadowBlur = 5;
+    context.arc(750, 900, 100, 0 * radian, 360 * radian, false);
+    context.stroke();
+    context.fill();
 
 }
 
